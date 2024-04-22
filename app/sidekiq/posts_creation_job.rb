5a111f434d0340e74c15e43fdb33276b50b7f2ae
liziane cysne <lizianecysne1@gmail.com>
2024-04-22 19:30:52 -0300

    feat: adiciona job do sidekiq

--- app/sidekiq/posts_creation_job.rb
+++ app/sidekiq/posts_creation_job.rb
@@ -1,13 +1,13 @@
 class PostsCreationJob
   include Sidekiq::Worker
 
-  def perform(user_id, file_path)
+  def perform(user_id, file_content)
     user = User.find(user_id)
     Rails.logger.info 'Iniciando a criação do post'
 
-    File.open(file_path, 'r').each_line do |line|
+    file_content.each_line do |line|
       title, content, tags = line.strip.split('|')
-      post = user.posts.build(title:, content:)
+      post = user.posts.build(title: title, content: content)
 
       tags.split(',').each do |tag_name|
         tag = Tag.find_or_create_by(name: tag_name.strip)
